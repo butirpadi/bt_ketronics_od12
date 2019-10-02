a53208a3d63b016f9f316fe6af77897f196f4d5d
butirpadi <butirpadi@gmail.com>
2019-10-03 06:52:06 +0700

    Fixing Report Picking List

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,6 @@ from . import res_company
 from . import account_invoice_line
 from . import account_invoice
 from . import product_template
-from . import stock_picking
-from . import stock_move
 from . import sale_order
+from . import stock_move
+from . import stock_picking
